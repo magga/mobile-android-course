e1e0d8ee401eaeb27df149553d46b6eb187d213d
Magga Panna <magga.panna@live.com>
2017-08-16 15:59:29 +0700

    Add Section 9 File 2

---
+++ 09-Flappy-Bird-Clone/02-Working-With-Sprites/core/src/id/magga/flappybird/FlappyBird.java
@@ -0,0 +1,45 @@
+package id.magga.flappybird;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class FlappyBird extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture background;
+
+    Texture[] birds;
+    int state = 0;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+
+		background = new Texture("bg.png");
+
+        birds = new Texture[2];
+
+        birds[0] = new Texture("bird.png");
+        birds[1] = new Texture("bird2.png");
+	}
+
+	@Override
+	public void render () {
+        batch.begin();
+
+        if(state == 0){
+            state = 1;
+        } else {
+            state = 0;
+        }
+
+        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+        batch.draw(birds[state], Gdx.graphics.getWidth() / 2 - birds[state].getWidth() / 2,
+                Gdx.graphics.getHeight() / 2 - birds[state].getHeight() / 2);
+
+        batch.end();
+	}
+}
